7e59fcdfa9ae1799b40ac450c8274833d37bafdc
Brett Kochendorfer <bkochendorfer@mozilla.com>
2022-07-19 11:37:31 -0500

    chore(ingress): Update ingress for security

--- mozmeao-fr/nucleus-dev/ingress-controller.sh
+++ mozmeao-fr/nucleus-dev/ingress-controller.sh
@@ -3,13 +3,9 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
-# We pin to helm chart version 3.36.0, which provides appVersion 0.49.0,
-# which is the latest release as of today that doesn't contain app 1.0+
-# with a breaking change to require kubeVersion 1.19+. -- 20210830 atoll
-
 HELMOPTIONS=$(cat << EOM
   --namespace $NS \
-  --version 3.36.0 \
+  --version 3.37.0 \
   -f $DEPLOYMENT/helm_configs/ingress.yml \
   nucleus-dev-ingress ingress-nginx/ingress-nginx
 EOM
